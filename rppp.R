7660be186e1ddf595d471eb00c0a5bec60229217
Jean Carlo Flores <jeancarlo.flores@avantica.net>
2018-09-18 15:52:39 -0500

    factors

--- rppp.R
+++ rppp.R
@@ -16,3 +16,21 @@ list1 <- list(c(2,5,3),21.3,sin)
 
 # Print the list.
 print(list1)
+
+# Create a matrix.
+M = matrix( c('a','a','b','c','b','a'), nrow = 2, ncol = 3, byrow = TRUE)
+print(M)
+
+# Create an array.
+a <- array(c('green','yellow'),dim = c(3,3,2))
+print(a)
+
+# Create a vector.
+apple_colors <- c('green','green','yellow','red','red','red','green')
+
+# Create a factor object.
+factor_apple <- factor(apple_colors)
+
+# Print the factor.
+print(factor_apple)
+print(nlevels(factor_apple))
