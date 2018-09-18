877437189865c00a8079ceaafd6305503daca127
Jean Carlo Flores <jeancarlo.flores@avantica.net>
2018-09-18 15:03:16 -0500

    listas

--- rppp.R
+++ rppp.R
@@ -1,2 +1,18 @@
 myString <- 'Hello world'
+v <- TRUE
+f <- FALSE
+vector1 <- list(c("1","2","3"), c("4","5","6"))
 print(myString)
+print(v)
+print(f)
+print(vector1[1])
+print(vector1[2])
+"This is a 
+comment"
+
+"A list is an R-object which can contain many different types of elements inside it like vectors, functions and even another list inside it."
+# Create a list.
+list1 <- list(c(2,5,3),21.3,sin)
+
+# Print the list.
+print(list1)
